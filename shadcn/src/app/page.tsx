106cf60f68601abb69123707dc47642024ceb081
0098-sayan <maitysayan704@gmail.com>
2025-09-23 11:09:01 +0530

    Basie next js and shadcn setup

---
+++ shadcn/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div className="flex items-center mt-20 justify-center">
+      <Button variant='ghost'>Click me</Button>
+    </div>
+  )
+}
